96bdda5ad21675b8153cd0a70a5b9faf52a2f225
gomfol12 <marek@am-hohenstein.de>
2022-02-02 15:46:47 +0100

    added patch xresources alt

--- config.def.h
+++ config.def.h
@@ -9,18 +9,29 @@ static int min_width = 500;                    /* minimum width when centered */
 
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"monospace:size=10",
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+static char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *symbol_1 = "<";
 static const char *symbol_2 = ">";
-static const char *colors[SchemeLast][2] = {
+
+static char normfgcolor[] = "#ffffff";
+static char normbgcolor[] = "#1a1a1a";
+static char selfgcolor[]  = "#ffffff";
+static char selbgcolor[]  = "#666666";
+static char selhighfgcolor[] = "#10a204";
+static char selhighbgcolor[] = "#666666";
+static char normhighfgcolor[] = "#10a204";
+static char normhighbgcolor[] = "#1a1a1a";
+
+static char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { normfgcolor, normbgcolor },
+	[SchemeSel]  = { selfgcolor,  selbgcolor  },
+	[SchemeSelHighlight] = { selhighfgcolor, selhighbgcolor },
+	[SchemeNormHighlight] = { normhighfgcolor, normhighbgcolor },
+	[SchemeOut]  = { "#000000",   "#000000" },
 };
 
 static const unsigned int alphas[SchemeLast][2] = {
@@ -43,3 +54,17 @@ static const char worddelimiters[] = " ";
 
 /* Size of the window border */
 static const unsigned int border_width = 5;
+
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+	{ "color7", STRING, &normfgcolor },
+	{ "background", STRING, &normbgcolor },
+	{ "color7",  STRING, &selfgcolor },
+	{ "color1",  STRING, &selbgcolor },
+    { "color2", STRING, &selhighfgcolor },
+    { "color1", STRING, &selhighbgcolor },
+    { "color2", STRING, &normhighfgcolor },
+    { "background", STRING, &normhighbgcolor },
+};
