75e7780ec4c765b7cbe93af75151bc39135fe917
gomfol12 <marek@am-hohenstein.de>
2022-10-02 16:25:19 +0200

    changed font

--- config.def.h
+++ config.def.h
@@ -9,8 +9,8 @@ static int min_width = 800;                    /* minimum width when centered */
 
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:pixelsize=16",
-    "Symbols Nerd Font:pixelsize=14:antialias=true:autohint=true",
+    "Iosevka:pixelsize=16:antialias=true:autohint=true",
+    "Symbols Nerd Font Mono:pixelsize=14:antialias=true:autohint=true",
     "Twemoji:pixelsize=14:antialias=true:autohint=true"
 };
 
